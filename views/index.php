b4cb69d3c8df891978aaac852faa0fecafd9e0fc
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-20 02:18:09 +0300

    feature: four-news block

--- views/index.php
+++ views/index.php
@@ -6,3 +6,28 @@
         <p class="hero-text"><?= htmlspecialchars(strip_tags($data['mainAnnounce'])) ?></p>
     </section>
 </main>
+
+<section class="news-header container">
+    <h1 class="news-section-title">Новости</h1>
+</section>
+
+<section class="four-news container">
+        <?php foreach ($data['fourNews'] as $news) : ?>
+            <article class="news-card">
+                <span class="news-card-date">
+                    <?= htmlspecialchars(date('d.m.Y', strtotime($news['date']))) ?>
+                </span>
+                <h2 class="news-card-title">
+                    <?= htmlspecialchars($news['title']) ?>
+                </h2>
+                <p class="news-card-text">
+                    <?= htmlspecialchars(strip_tags($news['announce'])) ?>
+                </p>
+                <a href="/new?id=" class="news-card-btn">
+                    Подробнее
+                    <img src="/images/Arrow1.svg" class="news-card-btn-img">
+                </a>
+            </article>
+        <?php endforeach; ?>
+</section>
+
