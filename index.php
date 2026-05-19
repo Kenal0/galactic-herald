5ff3d30548cd47515c8c640d27ccf72e381e0883
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-19 13:51:43 +0300

    feat: picture, text in index

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php require ('views/head.php') ?>
+
+<main class="main-content">
+    <section class="hero-banner container">
+        <h1 class="hero-title">Текст из бд будет здесь</h1>
+        <p class="hero-text">Описание из бд будет здесь</p>
+    </section>
+</main>
