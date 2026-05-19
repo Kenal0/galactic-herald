e040075fe2dac0cd813ab7d1a222d28987a55440
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-19 03:21:30 +0300

    feature: new folders, logo upload

---
+++ views/head.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Галактический вестник</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="main-header container">
+        <a href="/" class="logo-link">
+            <img src="images/Logo.svg" alt="Галактический вестник" class="logo-img">
+
+            <span class="logo-text">
+                Галактический<br>вестник
+            </span>
+        </a>
+    </header>
