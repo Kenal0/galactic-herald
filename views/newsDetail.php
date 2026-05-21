e8c4c16b25b6cdbe64588ec6f910992efeaba2d2
Kenal0 <kenal.karpenko.mikhail@gmail.com>
2026-05-21 14:49:19 +0300

    feature: news-detail picture, css styling

--- views/newsDetail.php
+++ views/newsDetail.php
@@ -7,7 +7,7 @@
         Главная
     </a>
     <span class="breadcrumbs-separator">
-        /
+         /
     </span>
     <span class="breadcrumbs-current">
         <?= $data['newsDetail']['title'] ?>
@@ -20,11 +20,12 @@
 
 <section class="news-section-detail">
     <div class="container">
+        <div class="news-detail-left">
         <span class="news-card-date">
             <?= htmlspecialchars(date('d.m.Y', strtotime($data['newsDetail']['date']))) ?>
         </span>
         <h2 class="news-detail-announce">
-        <?= $data['newsDetail']['announce'] ?>
+        <?= strip_tags($data['newsDetail']['announce']) ?>
         </h2>
         <div class="news-detail-content">
         <?= $data['newsDetail']['content'] ?>
@@ -32,6 +33,10 @@
         <a href="/" class="news-detail-back-btn">
             Назад к новостям
         </a>
+        </div>
+        <div class="news-detail-right">
+            <img src="/uploads/<?= $data['newsDetail']['image'] ?>" alt="Изображение к новости" class="news-detail-img">
+        </div>
     </div>
 </section>
 <?php require_once('views/partials/footer.php'); ?>
